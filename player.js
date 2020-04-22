59a5a5417608fb7a52e393dc818ef670e42a18ea
HylsonGlobal <hvescovi@gmail.com>
2020-04-22 16:30:03 -0300

    fix player

--- player.js
+++ player.js
@@ -2,6 +2,10 @@ currentSlide = 0; // current slide/line of the text file
 loaded = false; // the sequence file is not loaded yet
 lines = []
 
+// first values of x and y (centered)
+//x = $(window).width() / 2;
+//y = $(window).height() / 2;
+
 $.ajax({
     url: "sequence.txt",
     dataType: "text",
@@ -17,26 +21,22 @@ function loadSlide(i) {
     image = parts[0]
     msg = parts[1]
     xy = parts[2]
-    $('body').css("background-image", "url(/" + image);
-    if (i == 0) { // first message? centered.
-        x = $(window).width() / 2;
-        y = $(window).height() / 2;
-    } else { // place specified in the sequence
-        coords = xy.split(",")
-        x = coords[0]
-        y = coords[1]
-    }
-    $("#msg").css({ left: x, top: y }); // put the box in its place
+    $('body').css("background-image", "url('/" + image + "'");
+    //if (i > 0) { // is not the first time? update coordinates
+    coords = xy.split(",")
+    x = coords[0] + "px";
+    y = coords[1] + "px";
+    //}
+    $("#msg").css({ left: x, top: y, position: 'absolute' }); // put the box in its place
     $("#msg").html(msg);
     //alert(lines[i]);
 }
 
 $("#btn").click(function() {
-            if (loaded) {
-                if (currentSlide < lines.length {
-                        currentSlide = currentSlide + 1;
-                        loadSlide(currentSlide);
-                    }
-                }
-                // $("#msg").animate({ left: '250px' });
-            });
+    if (loaded) {
+        currentSlide = currentSlide + 1;
+        if (!(typeof lines[currentSlide] === 'undefined')) {
+            loadSlide(currentSlide);
+        }
+    }
+});
